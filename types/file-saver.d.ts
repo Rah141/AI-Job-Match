f423fa9fea451d583e8138fcc939a24437e555d6
Rah141 <133837255+Rah141@users.noreply.github.com>
2025-11-23 14:19:12 +0400

    Add file-saver type declarations to fix TypeScript build error

---
+++ types/file-saver.d.ts
@@ -0,0 +1,5 @@
+declare module 'file-saver' {
+  export function saveAs(blob: Blob, filename: string): void;
+  export function saveAs(blob: Blob, filename: string, options?: { autoBom?: boolean }): void;
+}
+
